bfe9afe222f1f2f0a282da172f410038d1274530
Khabibulix <unitetempete@gmail.com>
2023-02-23 10:28:51 +0100

    Beginning the implementation of sudoku

---
+++ Sudoku/Sudoku.java
@@ -0,0 +1,24 @@
+package Sudoku;
+
+import java.io.Serializable;
+
+public class Sudoku implements Serializable {
+    private final GameState gamestate;
+    private final int[][] gridState;
+
+    public static final int GRID_BOUNDARY = 9;
+
+    public SudokuGame(GameState gamestate, int[][]gridState){
+        this.gamestate = gameState;
+        this.gridState = gridState;
+    }
+
+    public GameState getGameState(){
+        return gameState;
+    }
+
+    public int[][] getGridState(){
+        return SudokuUtilities.copyToNewArray(gridState);
+    }
+
+}
